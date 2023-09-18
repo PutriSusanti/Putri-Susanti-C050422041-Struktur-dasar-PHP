d105590c964c44c05bf9b7fe0f4f8d82275d391e
Daisy <143997035+PutriSusanti@users.noreply.github.com>
2023-09-18 00:27:32 -0700

    Initial commit

---
+++ Bangunruangkerucut.php
@@ -0,0 +1,17 @@
+<?php
+
+$jari = 10;
+$selimut = 10;
+$phi = 3.14;
+
+$LuasAlas = $phi * $jari * $jari;
+$LuasSelimut = $phi * $jari * $selimut;
+$LuasPermukaan = $LuasAlas + $LuasSelimut;
+
+echo ("Luas alasnya adalah = ".$LuasAlas);
+echo("Luas Permukaan adalah = ".$LuasPermukaan);
+?>
+
+
+
+
---
+++ Biodata.php
@@ -0,0 +1,13 @@
+<?php
+$nim = 'C050422041';
+$nama = 'Putri Susanti;
+$alamat = 'Jl. Awang Sejahtera Rt.14';
+$nilai = 80;
+
+echo "NIM : ", $nim, "<br>";
+
+echo "Nama : ", $nama, "<br>";
+echo "Alamat : ", $alamat, "<br>";
+
+echo "Nilai : $nilai";
+?>
